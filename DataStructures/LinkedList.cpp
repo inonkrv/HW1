029299ef4192836fa766bc4ef032c3e997d22450
unknown <inonkrv@gmail.com>
2024-11-12 21:42:51 +0200

    fixed the file

--- DataStructures/LinkedList.cpp
+++ DataStructures/LinkedList.cpp
@@ -1,25 +1,6 @@
 #include "Stack.h"
 #include "LinkedList.h"
 #include <iostream>
-void push(Stack* s, unsigned int element)
-{
-	link* node = new link;
-	node = initL(element);
-	if (s->head == NULL)
-	{
-		s->head = node;
-	}
-	else
-	{
-		node->next = s->head;
-		s->head = node;//next
-	}
-}
-void initStack(Stack* s)
-{
-	s->head = new link;
-	s->head = NULL;
-}
 link* initL(int val)
 {
 	link* node = new link;
@@ -38,38 +19,4 @@ void printL(link* head)
 	}
 	delete curr;
 }
-void cleanStack(Stack* s)
-{
-	link* curr = s->head;
-	link* next = NULL;
-	while (curr != NULL)//going on all the list and removing all the nodes
-	{
-		next = curr->next;
-		delete curr;
-		curr = next;
-	}
-	s->head = NULL;
-}
-void deleteAll(Stack* s)
-{
-	delete s->head;
-	delete s;
-}
-int pop(Stack* s)// Return -1 if stack is empty
-{
-	link* node = new link;
-	if (s->head == NULL)
-	{
-		return -1;
-	}
-	if (s->head->next == NULL)
-	{
-		s->head = NULL;
-	}
-	else
-	{
-		node = s->head;
-		s->head = s->head->next;
-		delete node;
-	}
-}
+
