240ec36d39c5b3617d9fc1dfabeb09285bb7615a
unknown <inonkrv@gmail.com>
2024-11-12 18:46:27 +0200

    header

---
+++ DataStructures/LinkedList.h
@@ -0,0 +1,13 @@
+#include <cstddef>
+#ifndef LINKEDLISTH
+#define LINKEDLISTH
+
+typedef struct link
+{
+	struct link* next;
+	int value;
+} link;
+link* initL(int val);
+void printL(link* head);
+void freeL(link** head);
+#endif
